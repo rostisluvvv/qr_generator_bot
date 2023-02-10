4fea5f4e80d7d671f8af64da660a677e853728cb
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-02-10 09:33:53 +0300

    handling returns memory object

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,7 +1,25 @@
 from aiogram import Dispatcher
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, ContentType
+import URLValidator
 
 from lexicon.lexicon import LEXICON
-# from services.file_handling import generate_qr_code
+from services.file_handling import generate_qr_code
 
 
+async def process_start_command(message: Message):
+    await message.answer(LEXICON[message.text])
+
+
+async def process_help_command(message: Message):
+    await message.answer(LEXICON[message.text])
+
+
+async def process_url_command(message: Message):
+    with open(generate_qr_code(message), 'rb') as photo:
+        await message.answer_photo(photo)
+
+
+def register_user_handlers(dp: Dispatcher):
+    dp.register_message_handler(process_start_command, commands=['/start'])
+    dp.register_message_handler(process_help_command, commands=['/help'])
+    dp.register_message_handler(process_url_command)
--- services/file_handling.py
+++ services/file_handling.py
@@ -1,13 +1,17 @@
 import pyqrcode
-
+import io
 
 
 def generate_qr_code(url_link: str):
-    qr_code = pyqrcode.create(url_link)
-    qr_name = ''.join(e for e in url_link if e.isalnum())
-    qr_code.png(f'{qr_name}.png', scale=6)
-    return f'C:\Dev\8qr_generator_bot\services\{qr_name}.png'
+
+    url = pyqrcode.create(url_link)
+    buffer = io.BytesIO()
+    url.png(buffer)
+    photo = buffer.getvalue()
+    # qr_name = ''.join(e for e in url_link if e.isalnum())
+    return photo
 
 
+print(generate_qr_code('https://github.com/rostisluvvv'))
 
 
