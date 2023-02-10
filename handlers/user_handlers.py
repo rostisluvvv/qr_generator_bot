a54e5e77dbf8a9575bc9b7001d96273d685d7bb7
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-02-10 10:51:24 +0300

    sending via a memory cell does not work

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,6 +1,6 @@
 from aiogram import Dispatcher
 from aiogram.types import Message, ContentType
-import URLValidator
+
 
 from lexicon.lexicon import LEXICON
 from services.file_handling import generate_qr_code
@@ -15,11 +15,10 @@ async def process_help_command(message: Message):
 
 
 async def process_url_command(message: Message):
-    with open(generate_qr_code(message), 'rb') as photo:
-        await message.answer_photo(photo)
+    await message.answer_photo(generate_qr_code(message.url))
 
 
 def register_user_handlers(dp: Dispatcher):
-    dp.register_message_handler(process_start_command, commands=['/start'])
-    dp.register_message_handler(process_help_command, commands=['/help'])
-    dp.register_message_handler(process_url_command)
+    dp.register_message_handler(process_start_command, commands=['start'])
+    dp.register_message_handler(process_help_command, commands=['help'])
+    dp.register_message_handler(process_url_command, content_types=['url'])
--- services/file_handling.py
+++ services/file_handling.py
@@ -8,10 +8,9 @@ def generate_qr_code(url_link: str):
     buffer = io.BytesIO()
     url.png(buffer)
     photo = buffer.getvalue()
-    # qr_name = ''.join(e for e in url_link if e.isalnum())
     return photo
 
 
-print(generate_qr_code('https://github.com/rostisluvvv'))
+
 
 
